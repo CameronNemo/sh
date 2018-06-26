c669ad9df7c1466856ef388dc83ec14788aed446
Cameron Nemo <CameronNemo@users.noreply.github.com>
2018-06-25 23:21:35 -0700

    Now correctly deals with shell wildcard handling

--- debian.minimize.sh
+++ debian.minimize.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-which apt.minimize.sh >/dev/null 2>&1 && apt.minimize.sh || true
+command -v apt.minimize.sh >/dev/null 2>&1 && apt.minimize.sh || true
 
 test ! -f /etc/locale.gen || . /etc/locale.gen
 test ! -f /etc/default/locale || . /etc/default/locale
 test -z "$LANG" || excl_arg="! -name $(echo ${LANG} | cut -d_ -f1)*"
 
 find /usr/share/locale -mindepth 1 -maxdepth 1 \
-	"$locale_excl" ! -name "locale.alias" | xargs rm -rf || true
+	$excl_arg ! -name 'locale.alias' | xargs rm -rf || true
 
 find /usr/share/doc -depth -type f ! -name copyright | xargs rm -f || true
 find /usr/share/doc -empty | xargs rm -rf || true
